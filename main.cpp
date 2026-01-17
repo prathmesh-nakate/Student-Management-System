38440c92473b304852e0505e951155ce79e54be1
Prathmesh Nakate <prathmeshnakate24@gmail.com>
2026-01-17 15:01:22 +0530

    Day 3: feat: Implement file loading with auto-ID tracking

--- main.cpp
+++ main.cpp
@@ -32,6 +32,33 @@ struct Student
     }
 };
 
+vector <Student> students;
+int nextId = 1;
+
+void load_Students()
+{
+    ifstream file("students.txt");
+    if(!file.is_open()) 
+    return;
+
+    int maxId = 0;
+    Student s;
+
+    while(file >> s.id)
+    {
+        file.ignore();
+        getline(file, s.name);
+        file >> s.age;
+        file >> s.marks[0] >> s.marks[1] >> s.marks[2];
+
+        students.push_back(s);
+        if(s.id > maxId)
+        maxId = s.id;
+    }
+    nextId = maxId + 1;
+    file.close();
+}
+
 int main()
 {
     cout<<"Student management system";
